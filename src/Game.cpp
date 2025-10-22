ed5da21ec3c9594b16bef557bc2bc17341f7fc49
schnitzel0 <m.tamarova@uniyar.ac.ru>
2025-10-22 14:01:40 +0300

    Implement Game class with basic game loop methods

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,1 +1,22 @@
+#include "Game.h"
+#include <iostream>
+Game::Game() : isRunning(true) {
+    std::cout << "Game initialized" << std::endl;
+}
 
+void Game::run() {
+    while (isRunning) {
+        handleEvents();
+        update();
+        render();
+    }
+}
+
+void Game::handleEvents() {
+}
+
+void Game::update() {
+}
+
+void Game::render() {
+}
