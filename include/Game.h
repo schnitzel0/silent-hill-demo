5a377ff65a135f89d4f3314b877c239a6c2fdecd
schnitzel0 <m.tamarova@uniyar.ac.ru>
2025-10-22 13:58:35 +0300

    Add Game class header with basic game loop

--- include/Game.h
+++ include/Game.h
@@ -1,1 +1,15 @@
+#ifndef GAME_H
+#define GAME_H
 
+class Game {
+private:
+    bool isRunning;
+public:
+    Game();
+    void run();
+    void handleEvents();
+    void update();
+    void render();
+};
+
+#endif
